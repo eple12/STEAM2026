3830f9538525be03fa4a096ec902d96cb4cef37c
kms <marlon12@naver.com>
2026-04-07 19:39:57 +0900

    remove comments

--- main.py
+++ main.py
@@ -27,6 +27,3 @@ while True:
     action, _states = model.predict(obs, deterministic=True)
     obs, reward, done, truncated, info = env.step(action)
     env.render()
-
-# env.close()
-# asdfasdfasdfasf
